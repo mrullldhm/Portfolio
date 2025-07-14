f5b22bc5675ca657d031ea2cd071418efdfacbe7
mrullldhm <mrullldhm94@gmail.com>
2025-07-14 16:17:42 +0800

    Setup webstite design and metadata

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 bg-background">
+      <div className="max-w-3xl mx-auto flex flex-wrap justify-between gap-3 px-3 py-5">
+        <nav className="space-x-4 font-medium">
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/social">Social Media</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
